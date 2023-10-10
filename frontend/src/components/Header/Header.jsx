74eb4fc0b9f1f782ba94c25fb22e6da53e121e09
Kong-E <kng001016@dgu.ac.kr>
2023-10-11 03:46:05 +0900

    feat: Header 클래스네임 수정

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,33 +1,32 @@
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import styles from './Header.module.scss';
+import classNames from 'classnames';
 
 export const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const path = location.pathname;
+  const path = location.pathname.split('/')[1];
 
   return (
     <>
       <header>
         <img src={logo} alt="logo" className={styles.logo} />
         <ul className={styles.list}>
-          <li className={`${styles.item} ${path === '/' ? styles.selected : ''}`} onClick={() => navigate('/')}>
+          <li className={classNames(styles.item, path === '' ? styles.active : '')} onClick={() => navigate('/')}>
             공지사항
           </li>
           <li
-            className={`${styles.item} ${path === '/schoolCalendar' ? styles.selected : ''}`}
+            className={classNames(styles.item, path === 'schoolCalendar' ? styles.active : '')}
             onClick={() => navigate('/schoolCalendar')}>
             학사일정
           </li>
           <li
-            className={`${styles.item} ${path === '/myPage' ? styles.selected : ''}`}
+            className={classNames(styles.item, path === 'myPage' ? styles.active : '')}
             onClick={() => navigate('/myPage')}>
             마이페이지
           </li>
-          <li
-            className={`${styles.item} ${path === '/login' ? styles.selected : ''}`}
-            onClick={() => navigate('/login')}>
+          <li className={classNames(styles.item, styles.active)} onClick={() => navigate('/login')}>
             로그인
           </li>
         </ul>
