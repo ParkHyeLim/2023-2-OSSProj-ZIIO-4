f27f1ace182da1eefa38579af47db506456ae14a
bbbang105 <2018111366@dgu.ac.kr>
2023-11-05 16:23:31 +0900

    #6 feat : 매일 오후 8시에 크롤링을 실행하는 기능 추가

--- backend/src/main/java/com/ziio/backend/crawling/CrawlerManager.java
+++ backend/src/main/java/com/ziio/backend/crawling/CrawlerManager.java
@@ -1,4 +1,8 @@
-package com.ziio.backend.service.crawling;
+package com.ziio.backend.crawling;
+
+import com.ziio.backend.service.AcademicService;
+import com.ziio.backend.service.NoticeService;
+import org.springframework.scheduling.annotation.Scheduled;
 
 public class CrawlerManager {
     private MainWebsiteCrawler mainWebsiteCrawler;
@@ -6,31 +10,26 @@ public class CrawlerManager {
     private AcademicCalendarWebsiteCrawler academicCalendarWebsiteCrawler;
     private EtcWebsiteCrawler etcWebsiteCrawler;
 
-    public CrawlerManager() {
+    public CrawlerManager(AcademicService academicService, NoticeService noticeService) {
         mainWebsiteCrawler = new MainWebsiteCrawler();
         collegeAndDepartmentWebsiteCrawler = new CollegeAndDepartmentWebsiteCrawler();
-        academicCalendarWebsiteCrawler = new AcademicCalendarWebsiteCrawler();
-        etcWebsiteCrawler = new EtcWebsiteCrawler();
+        academicCalendarWebsiteCrawler = new AcademicCalendarWebsiteCrawler(academicService);
+        //etcWebsiteCrawler = new EtcWebsiteCrawler(noticeService);
     }
-
+    @Scheduled(cron = "0 0 20 * * ?") // 매일 오후 8시에 크롤링 실행
     public void runAllCrawlers() {
         // 각 크롤러 실행
-        mainWebsiteCrawler.crawl();
-        collegeAndDepartmentWebsiteCrawler.crawl();
+        //mainWebsiteCrawler.crawl();
+        //collegeAndDepartmentWebsiteCrawler.crawl();
         academicCalendarWebsiteCrawler.crawl();
-        etcWebsiteCrawler.crawl();
+        //etcWebsiteCrawler.crawl();
 
         // 크롤링된 데이터 처리 로직
-        processData();
+        //processData();
     }
 
     private void processData() {
         // 크롤링된 데이터 처리 코드 작성
         // 각 크롤러 클래스의 결과를 병합하거나 원하는 형태로 가공할 수 있습니다.
-    }
-
-    public static void main(String[] args) {
-        CrawlerManager manager = new CrawlerManager();
-        manager.runAllCrawlers();
     }
 }
