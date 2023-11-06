69a49032161d51fb9efc40e5fecebfc4ba7fea55
bbbang105 <2018111366@dgu.ac.kr>
2023-11-06 19:47:05 +0900

    #13 feat : 컨트롤러 기능 구현
    - 애플리케이션 실행 시, 자동으로 크롤링 진행

---
+++ backend/src/main/java/com/ziio/backend/controller/AcademicController.java
@@ -0,0 +1,22 @@
+package com.ziio.backend.controller;
+
+import com.ziio.backend.crawling.CrawlerManager;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AcademicController {
+    private final CrawlerManager crawlerManager;
+
+    @Autowired
+    public AcademicController(CrawlerManager crawlerManager) {
+        this.crawlerManager = crawlerManager;
+    }
+
+    @PostConstruct
+    public void startCrawling() {
+        // 크롤링 실행
+        crawlerManager.runAcademicCrawlers();
+    }
+}
