84399fb5c1abc87a9ce59cdc02bab2aab0266f00
bbbang105 <2018111366@dgu.ac.kr>
2023-11-19 10:43:16 +0900

    #20 style : 어노테이션 추가
    - NOT NULL 설정을 위한 @column 어노테이션
    - lombok @Getter @Setter 어노테이션

--- backend/src/main/java/com/ziio/backend/entity/Academic.java
+++ backend/src/main/java/com/ziio/backend/entity/Academic.java
@@ -1,50 +1,24 @@
 package com.ziio.backend.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
 @Entity
 public class Academic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // id 자동 +1
     private Long id;
 
+    @Column(nullable = false)
     private String date;
+
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String host_department;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getHost_department() {
-        return host_department;
-    }
-
-    public void setHost_department(String host_department) {
-        this.host_department = host_department;
-    }
 }
--- backend/src/main/java/com/ziio/backend/entity/User.java
+++ backend/src/main/java/com/ziio/backend/entity/User.java
@@ -3,10 +3,7 @@ package com.ziio.backend.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,6 +13,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String name;
 }
