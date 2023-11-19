85c57ff49f384305498fa589b783444a7b70e49e
bbbang105 <2018111366@dgu.ac.kr>
2023-11-19 10:45:03 +0900

    #20 feat : Color 엔터티 & 레포지토리 생성

---
+++ backend/src/main/java/com/ziio/backend/entity/Color.java
@@ -0,0 +1,26 @@
+package com.ziio.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Getter
+@Setter
+@Entity
+public class Color {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+}
