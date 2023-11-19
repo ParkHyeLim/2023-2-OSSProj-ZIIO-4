64e86251fee0aad53ef3d5483c326b49786004c3
bbbang105 <2018111366@dgu.ac.kr>
2023-11-19 10:45:16 +0900

    #20 feat : Bookmark 엔터티 & 레포지토리 생성

---
+++ backend/src/main/java/com/ziio/backend/entity/Bookmark.java
@@ -0,0 +1,26 @@
+package com.ziio.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Getter
+@Setter
+@Entity
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private Long categoryId;
+}
