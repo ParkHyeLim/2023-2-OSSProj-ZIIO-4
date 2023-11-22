6e9a2ff26ad5f4860f4061d185ff2dd427be82e6
bbbang105 <2018111366@dgu.ac.kr>
2023-11-22 15:57:33 +0900

    #31 style : ResponseEntity를 사용하도록 변경

--- backend/src/main/java/com/ziio/backend/controller/AcademicController.java
+++ backend/src/main/java/com/ziio/backend/controller/AcademicController.java
@@ -3,6 +3,8 @@ package com.ziio.backend.controller;
 import com.ziio.backend.entity.Academic;
 import com.ziio.backend.service.AcademicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,13 @@ public class AcademicController {
     @Autowired
     private AcademicService academicService;
 
+    //
     @GetMapping
-    public List<Academic> getAllAcademics() {
-        // 모든 학사 일정을 불러와 반환
-        return academicService.getAllAcademics();
+    public ResponseEntity<List<Academic>> getAllAcademics() {
+        List<Academic> academics = academicService.getAllAcademics();
+        return new ResponseEntity<>(academics, HttpStatus.OK);
     }
+
 }
 
 
