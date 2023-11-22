4c07a6d84507ff4de570e35c63370cbf63c500f4
bbbang105 <2018111366@dgu.ac.kr>
2023-11-23 01:42:39 +0900

    style : 어노테이션 및 주석 수정

--- backend/src/main/java/com/ziio/backend/dto/UserDTO.java
+++ backend/src/main/java/com/ziio/backend/dto/UserDTO.java
@@ -1,13 +1,11 @@
 package com.ziio.backend.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 public class UserDTO {
 
     @Getter
-    @AllArgsConstructor
     @Builder
     public static class Info {
         private String email;
