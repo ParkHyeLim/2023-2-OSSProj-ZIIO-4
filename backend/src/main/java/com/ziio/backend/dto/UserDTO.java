f314498bfe24e152f259109cec56c9fdb15f0129
bbbang105 <2018111366@dgu.ac.kr>
2023-11-26 01:20:57 +0900

    #21 style : 액세스 토큰 타입 String으로 변경

--- backend/src/main/java/com/ziio/backend/dto/UserDTO.java
+++ backend/src/main/java/com/ziio/backend/dto/UserDTO.java
@@ -2,7 +2,6 @@ package com.ziio.backend.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 
 public class UserDTO {
 
@@ -11,6 +10,6 @@ public class UserDTO {
     public static class Info {
         private String email;
         private String name;
-        private OAuth2AccessToken accessToken;
+        private String accessToken;
     }
 }
--- backend/src/main/java/com/ziio/backend/entity/User.java
+++ backend/src/main/java/com/ziio/backend/entity/User.java
@@ -2,7 +2,6 @@ package com.ziio.backend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 
 import javax.persistence.*;
 
@@ -21,5 +20,5 @@ public class User {
     private String name;
     
     @Column(nullable = false)
-    private OAuth2AccessToken accessToken;
+    private String accessToken;
 }
