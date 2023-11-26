793274c7d04f5c591d1120df85989bc3137b84ea
bbbang105 <2018111366@dgu.ac.kr>
2023-11-27 01:57:49 +0900

    #40 feat : 모든 정보를 반환하는 메소드 추가

--- backend/src/main/java/com/ziio/backend/service/NoticeService.java
+++ backend/src/main/java/com/ziio/backend/service/NoticeService.java
@@ -5,6 +5,8 @@ import com.ziio.backend.repository.NoticeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class NoticeService {
@@ -18,5 +20,10 @@ public class NoticeService {
     // DB에 공지사항 저장
     public void save(Notice notice) {
         noticeRepository.save(notice);
+    }
+
+    // 모든 공지사항 정보를 반환하는 메소드
+    public List<Notice> getAllNotices() {
+        return noticeRepository.findAll();
     }
 }
