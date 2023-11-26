c609af36b16ba25a92118e9d623dae3a2060b547
bbbang105 <2018111366@dgu.ac.kr>
2023-11-27 01:58:11 +0900

    #40 feat : 공지사항 첫 화면 API 구현

--- backend/src/main/java/com/ziio/backend/controller/NoticeController.java
+++ backend/src/main/java/com/ziio/backend/controller/NoticeController.java
@@ -1,7 +1,39 @@
 package com.ziio.backend.controller;
 
-import org.springframework.stereotype.Controller;
+import com.ziio.backend.entity.Category;
+import com.ziio.backend.entity.Notice;
+import com.ziio.backend.service.CategoryService;
+import com.ziio.backend.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/notices")
 public class NoticeController {
+
+    @Autowired
+    private NoticeService noticeService;
+    @Autowired
+    private CategoryService categoryService;
+
+    // 모든 공지사항을 반환하는 메소드(= 공지사항 첫 화면)
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getAllNotices() {
+        List<Notice> notices = noticeService.getAllNotices();
+        List<Category> categories = categoryService.getAllCategories();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("notices", notices);
+        response.put("categories", categories);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
