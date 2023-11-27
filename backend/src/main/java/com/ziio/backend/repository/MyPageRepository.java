08c83431b36d1887fb246f66b104055c4aa84a9d
bbbang105 <2018111366@dgu.ac.kr>
2023-11-28 03:20:49 +0900

    #40 feat : 마이페이지에 특정 공지사항을 추가하는 기능 구현

--- backend/src/main/java/com/ziio/backend/repository/MyPageRepository.java
+++ backend/src/main/java/com/ziio/backend/repository/MyPageRepository.java
@@ -10,4 +10,8 @@ public interface MyPageRepository extends JpaRepository<MyPage, Long> {
     // 사용자의 이메일과 학사일정 id를 기준으로 개수를 카운트하는 메소드
     @Query("SELECT COUNT(mp) FROM MyPage mp WHERE mp.user_email = :userEmail AND mp.academic_id = :academicId")
     long countByUserEmailAndAcademicId(@Param("userEmail") String userEmail, @Param("academicId") Long academicId);
+
+    // 사용자의 이메일과 공지사항 id를 기준으로 개수를 카운트하는 메소드
+    @Query("SELECT COUNT(mp) FROM MyPage mp WHERE mp.user_email = :userEmail AND mp.notice_id = :noticeId")
+    long countByUserEmailAndNoticeId(@Param("userEmail") String userEmail, @Param("noticeId") Long noticeId);
 }
--- backend/src/main/java/com/ziio/backend/service/MyPageService.java
+++ backend/src/main/java/com/ziio/backend/service/MyPageService.java
@@ -1,7 +1,9 @@
 package com.ziio.backend.service;
 
+import com.ziio.backend.dto.NoticeDTO;
 import com.ziio.backend.entity.Academic;
 import com.ziio.backend.entity.MyPage;
+import com.ziio.backend.entity.Notice;
 import com.ziio.backend.exception.DuplicateRecordException;
 import com.ziio.backend.repository.MyPageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +37,29 @@ public class MyPageService {
         } else {
             // 중복인 경우
             throw new DuplicateRecordException("This academic is already added to the MyPage.");
+        }
+    }
+
+    // 마이페이지에 특정 공지사항을 추가하는 메소드
+    public void addNoticeToMyPage(Notice notice, NoticeDTO.Request request, String userEmail) {
+        // 중복 체크
+        long count = myPageRepository.countByUserEmailAndNoticeId(userEmail, request.getNotice_id());
+        // 중복이 아닌 경우
+        if (count == 0) {
+            MyPage myPage = new MyPage();
+            myPage.setNotice_id(notice.getNotice_id());
+            myPage.setUser_email(userEmail);
+            myPage.setStart_date(request.getStart_date());
+            myPage.setEnd_date(request.getEnd_date());
+            myPage.setTitle(notice.getTitle());
+            myPage.setColor_code(request.getColor_code());
+            myPage.setUrl(notice.getUrl());
+            myPage.setMemo(request.getMemo());
+
+            myPageRepository.save(myPage);
+        } else {
+            // 중복인 경우
+            throw new DuplicateRecordException("This notice is already added to the MyPage.");
         }
     }
 }
