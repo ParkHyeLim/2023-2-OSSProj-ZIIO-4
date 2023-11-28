3725628132b1dd7e9b569c1110cfac8154afe23e
bbbang105 <2018111366@dgu.ac.kr>
2023-11-29 02:37:21 +0900

    #40 feat : 이메일과 학사일정 id로 찾아 정보를 반환하는 기능 구현

--- backend/src/main/java/com/ziio/backend/repository/MyPageRepository.java
+++ backend/src/main/java/com/ziio/backend/repository/MyPageRepository.java
@@ -18,4 +18,8 @@ public interface MyPageRepository extends JpaRepository<MyPage, Long> {
     // 사용자의 이메일과 공지사항 id로 찾아 정보를 반환하는 메소드
     @Query("SELECT mp FROM MyPage mp WHERE mp.user_email = :userEmail AND mp.notice_id = :noticeId")
     MyPage findByUserEmailAndNoticeId(@Param("userEmail") String userEmail, @Param("noticeId") Long noticeId);
+
+    // 사용자의 이메일과 학사일정 id로 찾아 정보를 반환하는 메소드
+    @Query("SELECT mp FROM MyPage mp WHERE mp.user_email = :userEmail AND mp.academic_id = :academicId")
+    MyPage findByUserEmailAndAcademicId(@Param("userEmail") String userEmail, @Param("academicId") Long academicId);
 }
