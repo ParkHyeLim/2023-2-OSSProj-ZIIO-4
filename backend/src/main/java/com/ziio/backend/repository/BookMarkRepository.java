2f1276b1b8557ea64e4930926915b2a37fea1a27
bbbang105 <2018111366@dgu.ac.kr>
2023-12-02 11:42:19 +0900

    #40 feat : 이메일, 카테고리 id 기준으로 개수 파악 & 삭제하는 메소드 구현

--- backend/src/main/java/com/ziio/backend/repository/BookMarkRepository.java
+++ backend/src/main/java/com/ziio/backend/repository/BookMarkRepository.java
@@ -2,6 +2,17 @@ package com.ziio.backend.repository;
 
 import com.ziio.backend.entity.BookMark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
+    // 사용자의 이메일과 카테고리 id를 기준으로 개수를 카운트하는 메소드
+    @Query("SELECT COUNT(bm) FROM BookMark bm WHERE bm.user_email = :userEmail AND bm.category_id = :categoryId")
+    long countByUserEmailAndCategoryId(@Param("userEmail") String userEmail, @Param("categoryId") String categoryId);
+
+    // 사용자의 이메일과 카테고리 id를 기준으로 삭제하는 메소드
+    @Modifying
+    @Query("DELETE FROM BookMark bm WHERE bm.user_email = :userEmail AND bm.category_id = :categoryId")
+    void deleteByUserEmailAndCategoryId(@Param("userEmail") String userEmail, @Param("categoryId") String categoryId);
 }
