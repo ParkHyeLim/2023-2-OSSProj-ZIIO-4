ac5f3baa24f72a309866b728e7f3f69f06751153
bbbang105 <2018111366@dgu.ac.kr>
2023-12-05 18:21:01 +0900

    #59 style : RDS상 호스트로 변경

--- backend/src/main/java/com/ziio/backend/crawler/CrawlerManager.java
+++ backend/src/main/java/com/ziio/backend/crawler/CrawlerManager.java
@@ -5,6 +5,8 @@ import com.ziio.backend.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class CrawlerManager {
     @Autowired
