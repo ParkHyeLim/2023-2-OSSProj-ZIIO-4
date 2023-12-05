da35da5a47e8bd2b33f88aa70635bd15816a9efb
bbbang105 <2018111366@dgu.ac.kr>
2023-12-05 19:52:58 +0900

    # 59 feat : 마이페이지 GET 기능 구현 & PATCH 반환 데이터 수정

--- backend/src/main/java/com/ziio/backend/controller/MyPageController.java
+++ backend/src/main/java/com/ziio/backend/controller/MyPageController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,21 +24,38 @@ public class MyPageController {
 
     // 사용자의 모든 마이페이지 일정을 반환하는 메소드(= 마이페이지 첫 화면)
     @GetMapping
-    public ResponseEntity<List<MyPage>> getAllMyPages(
+    public ResponseEntity<List<MyPageDTO.GetResponse>> getUserMyPages(
             @RequestHeader("Authorization") String authorizationHeader) {
 
         // 토큰에서 유저 이메일 가져오기
         String jwtToken = jwtUtil.getJwtTokenFromHeader(authorizationHeader);
         String userEmail = jwtUtil.getEmailFromToken(jwtToken);
 
+        // 사용자의 마이페이지 목록
         List<MyPage> myPages = myPageService.getAllMyPagesByUserEmail(userEmail);
 
-        return new ResponseEntity<>(myPages, HttpStatus.OK);
+        // 응답 객체 생성 및 반환
+        List<MyPageDTO.GetResponse> response = new ArrayList<>();
+        for (MyPage mypage : myPages) {
+            MyPageDTO.GetResponse res = MyPageDTO.GetResponse.builder()
+                    .my_page_id(mypage.getMy_page_id())
+                    .start_date(mypage.getStart_date())
+                    .end_date(mypage.getEnd_date())
+                    .title(mypage.getTitle())
+                    .url(mypage.getUrl())
+                    .color_code(mypage.getColor_code())
+                    .memo(mypage.getMemo())
+                    .build();
+
+            response.add(res);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 사용자의 요청에 따라 일정을 수정하는 메소드
-    @PatchMapping("/edits")
-    public ResponseEntity<MyPageDTO.Info> updateMyPage(
+    @PatchMapping
+    public ResponseEntity<MyPageDTO.PostResponse> updateMyPage(
             @RequestHeader("Authorization") String authorizationHeader,
             @RequestBody MyPageDTO.Request request) {
 
@@ -55,12 +73,8 @@ public class MyPageController {
         MyPage updatedMyPage = myPageService.updateMyPage(myPageId, request, userEmail);
 
         // 응답 객체 생성 및 반환
-        MyPageDTO.Info updatedMyPageResponse = MyPageDTO.Info.builder()
+        MyPageDTO.PostResponse updatedMyPageResponse = MyPageDTO.PostResponse.builder()
                 .my_page_id(myPageId)
-                .notice_id(updatedMyPage.getNotice_id())
-                .academic_id(updatedMyPage.getAcademic_id())
-                .category_id(updatedMyPage.getCategory_id())
-                .user_email(updatedMyPage.getUser_email())
                 .start_date(updatedMyPage.getStart_date())
                 .end_date(updatedMyPage.getEnd_date())
                 .title(updatedMyPage.getTitle())
