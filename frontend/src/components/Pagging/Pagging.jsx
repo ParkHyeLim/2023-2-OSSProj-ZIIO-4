4b10e5e19171b69443e0e6e8196af2c8eee529df
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-12-08 18:13:35 +0900

    [#9] feat: 무한스크롤 취소, 데이터가 없을 시 화면 추가

--- frontend/src/components/Pagging/Pagging.jsx
+++ frontend/src/components/Pagging/Pagging.jsx
@@ -1,93 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { debounce } from 'lodash';
+import { LuDatabase } from "react-icons/lu";
 import styles from './Pagging.module.scss';
 import NoticeItem from '../NoticeItem/NoticeItem';
 
 const Pagging = ({ data, noticeCategory, changeClipStarNotice }) => {
-  const [noticeData, setNoticeData] = useState([]);
-  const [nowData, setNowData] = useState([{ author: '', category_id: '', date_posted: '', id: 0, title: '', url: '' }]);
-  const [nowPage, setNowPage] = useState(1);
+  const [noticeData, setNoticeData] = useState([{ author: '', category_id: '', date_posted: '', id: 0, title: '', url: '' }]);
   const [nowFixedNuber, setNowFixedNuber] = useState(0);
 
   useEffect(() => {
+    const fixedData = [];
+    const nonFixedData = [];
     if (Array.isArray(data)) {
-      setNowPage(1);
-      const inputData = [...data];
-      const fixedData = inputData.filter((item) => {
-        return item.fixed === true;
+      data.forEach(item => {
+        if (item.fixed === true) fixedData.push(item);
+        else nonFixedData.push(item);
       });
       setNowFixedNuber(fixedData.length);
-      const updatedInputData = inputData.filter((item) => {
-        return item.fixed !== true;
-      });
-      const mergedArray = fixedData.concat(updatedInputData);
+      const mergedArray = fixedData.concat(nonFixedData);
       setNoticeData(mergedArray);
-      setNowData(mergedArray.slice(0, 10));
     }
   }, [data]);
-
-  useEffect(() => {
-    if (nowData.length === 10) {
-      const container = document.querySelector(`.${styles.container}`);
-      const handleScroll = debounce(() => {
-        const container = document.querySelector(`.${styles.container}`);
-        const scrollHeight = container.scrollHeight;
-        const scrollTop = container.scrollTop;
-        const clientHeight = container.offsetHeight;
-
-        if (scrollTop + clientHeight >= scrollHeight) {
-          fetchMoreData();
-        }
-      }, 100);
-      container.addEventListener('scroll', handleScroll);
-      return () => {
-        container.removeEventListener('scroll', handleScroll);
-      };
-    }
-  }, [nowData]);
-
-  useEffect(() => {
-    if (nowPage > 1) {
-      const container = document.querySelector(`.${styles.container}`);
-      const handleScroll = debounce(() => {
-        const container = document.querySelector(`.${styles.container}`);
-        const scrollHeight = container.scrollHeight - 100;
-        const scrollTop = container.scrollTop;
-        const clientHeight = container.offsetHeight;
-
-        if (scrollTop + clientHeight >= scrollHeight) {
-          fetchMoreData();
-        }
-      }, 100);
-      container.addEventListener('scroll', handleScroll);
-      return () => {
-        container.removeEventListener('scroll', handleScroll);
-      };
-    }
-  }, [nowPage]);
-
-  const fetchMoreData = () => {
-    if (nowPage >= Math.ceil(noticeData.length / 10)) {
-      return;
-    }
-    const cutNum = 10 * nowPage;
-    const nextPageData = noticeData.slice(cutNum, cutNum + 10);
-    setNowPage(prev => prev + 1);
-    const mergedArray = nowData.concat(nextPageData);
-    setNowData(mergedArray);
-  };
-
+  
   return (
     <div className={styles.container}>
-      {nowData.map((value, index) => (
-        <NoticeItem
-          key={index}
-          item={value}
-          itemNumber={index - nowFixedNuber + 1}
-          category={noticeCategory}
-          onClipClick={item => changeClipStarNotice(item)}
-        />
-      ))}
+      {noticeData.length === 0 ?
+        <div className={styles.NonoDataContainer}>
+          <LuDatabase className={styles.NonoDataImage} size={20} color="#ea5514" />
+          <div className={styles.NonoDataText}>불러온 공지사항이 없습니다.</div>
+        </div>
+        : (noticeData.map((value, index) => (
+          <NoticeItem
+            key={index}
+            item={value}
+            itemNumber={index - nowFixedNuber + 1}
+            category={noticeCategory}
+            onClipClick={item => changeClipStarNotice(item)}
+          />
+        ))
+        )
+      }
     </div>
   );
 };
