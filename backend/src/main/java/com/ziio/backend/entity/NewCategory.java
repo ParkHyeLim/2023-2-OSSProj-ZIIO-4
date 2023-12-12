5a2c8bd7f8326e93a4a6d1d33c4cac255bd78eac
bbbang105 <2018111366@dgu.ac.kr>
2023-12-13 02:15:49 +0900

    # 65 feat : 크롤링한 새로운 공지사항을 저장하기 위한 테이블 생성

---
+++ backend/src/main/java/com/ziio/backend/entity/NewCategory.java
@@ -0,0 +1,25 @@
+용package com.ziio.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+// 크롤링 후, 기존 카테고리를 대체하기 위한 테이블
+@Getter
+@Setter
+@Entity
+public class NewCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id 자동 +1
+    private Long id;
+
+    @Column(nullable = false)
+    private String category_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int top_fixed;
+}
